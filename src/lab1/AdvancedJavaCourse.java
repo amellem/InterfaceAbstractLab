f0400f13c26347956ca5f2ed77a29a0dd6a25a25
amellem <amellem@L11105042790.wctc.msft>
2016-09-20 15:07:25 -0500

    InterfaceAbstractLab - complete

---
+++ src/lab1/AdvancedJavaCourse.java
@@ -0,0 +1,23 @@
+package lab1;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Describe responsibilities here.
+ *
+ * @author      your name goes here
+ * @version     1.00
+ */
+public class AdvancedJavaCourse extends ProgrammingCourse {
+
+    private double credits;
+    
+    @Override
+    public void setCredits(double credits) {
+        this.credits=credits;
+    }
+    
+
+    
+    
+}
---
+++ src/lab1/IntroJavaCourse.java
@@ -0,0 +1,17 @@
+package lab1;
+
+/**
+ * Describe responsibilities here.
+ *
+ * @author      your name goes here
+ * @version     1.00
+ */
+public class IntroJavaCourse extends ProgrammingCourse {
+   private double credits;
+    
+    @Override
+    public void setCredits(double credits) {
+        this.credits=credits;
+    }
+
+}
